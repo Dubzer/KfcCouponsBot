e516c8c35e0ab7b1f873314771fa28e700fb1d17
Dubzer <dubzer@ya.ru>
2019-12-03 20:37:01 +0300

    Added Post method to Poster

--- KfcCoupons/Poster.cs
+++ KfcCoupons/Poster.cs
@@ -1,14 +1,21 @@
-﻿using Telegram.Bot;
+﻿using KfcCoupons.Models;
+using Telegram.Bot;
+using Telegram.Bot.Types;
 
 namespace KfcCoupons
 {
     public class Poster
     {
-        private TelegramBotClient _client;
-        
-        public Poster(TelegramBotClient client)
+        private readonly ChatId _chatId;
+        private readonly TelegramBotClient _client;
+
+        public Poster(TelegramBotClient client, ChatId chatId) =>
+            (_client, _chatId) = (client, chatId);
+
+        public void Post(Product product)
         {
-            _client = client;
+            string text = $"{product.Description}\n\nКупон: `{product.Coupon}`\nНовая цена: {product.Price}\nДешевле на: {product.OldPrice - product.Price}";
+            _client.SendTextMessageAsync(_chatId, text);
         }
     }
 }
