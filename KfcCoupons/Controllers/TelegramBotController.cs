c02e402c30a085fb53591bae254d04f853e807d2
Dubzer <dubzer@ya.ru>
2019-12-03 18:02:19 +0300

    Added main controller

---
+++ KfcCoupons/Controllers/TelegramBotController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KfcCoupons.Controllers
+{
+    [Route("api/[controller]")]
+    public class TelegramBotController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+    }
+}
