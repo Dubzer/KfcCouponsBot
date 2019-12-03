14f02f5cf1ca3b5b6dfb9ff7ee3008d94be7354f
Dubzer <dubzer@ya.ru>
2019-12-03 20:37:18 +0300

    Updated Product model

--- KfcCoupons/Models/Product.cs
+++ KfcCoupons/Models/Product.cs
@@ -4,24 +4,25 @@ namespace KfcCoupons.Models
 {
     public class Product
     {
-        [JsonProperty("price")]
-        private Price _price;
-
         [JsonProperty("productId")]
         public long Id { get; set; }
 
+        [JsonProperty("price")]
+        private Price _price;
+        public long Price => _price.Amount;
+        
+        [JsonProperty("oldPrice")]
+        public long OldPrice { get; set; }
+        
         // TODO: Convert to enum
         [JsonProperty("type")]
         public string Type { get; set; }
-
+        
         [JsonProperty("translation")]
-        public Translation Translation { get; set; }
-
-        public long Price => _price.Amount;
-
-        [JsonProperty("oldPrice")]
-        public long OldPrice { get; set; }
-
+        private Translation Translation { get; set; }
+        public string Description => Translation.Ru.Description;
+        public uint Coupon => Translation.Ru.Short;
+        
         // TODO: Media
         //[JsonProperty("media")]
         //public Media Media { get; set; }
