4dee039332f1605e3970d9839c7d460a35d4226b
Dubzer <dubzer@ya.ru>
2019-12-03 18:25:43 +0300

    Added poster class

---
+++ KfcCoupons/Poster.cs
@@ -0,0 +1,14 @@
+﻿using Telegram.Bot;
+
+namespace KfcCoupons
+{
+    public class Poster
+    {
+        private TelegramBotClient _client;
+        
+        public Poster(TelegramBotClient client)
+        {
+            _client = client;
+        }
+    }
+}
