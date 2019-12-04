bd77a2ab55b12e30bb77a2dc25edaa8818ad1407
Dubzer <dubzer@ya.ru>
2019-12-05 00:18:18 +0300

    Implemented Cleaner

--- KfcCoupons/Cleaner.cs
+++ KfcCoupons/Cleaner.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using KfcCoupons.Models;
 using Telegram.Bot;
@@ -14,9 +15,13 @@ namespace KfcCoupons
         public Cleaner(TelegramBotClient client,ChatId chatId) =>
             (_client, _chatId) = (client, chatId);
 
-        public async Task Clean(Dictionary<long, int> messageIdOfProduct, Product[] currentProducts)
+        public async Task Clean(Dictionary<long, int> messageIdOfProduct, IEnumerable<Product> currentProducts)
         {
-            
+            foreach (KeyValuePair<long, int> product in messageIdOfProduct.Where(product => 
+                !currentProducts.Select(p => p.Id).Contains(product.Key)))
+            {
+                await _client.DeleteMessageAsync(_chatId, product.Value);
+            }
         }
     }
 }
