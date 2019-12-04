bf80be861b6dead221da96ca396aadbbfe52a83c
Dubzer <dubzer@ya.ru>
2019-12-04 23:31:20 +0300

    Added DescriptionGenerator

---
+++ KfcCoupons/DescriptionGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using KfcCoupons.Models;
+
+namespace KfcCoupons
+{
+    public class DescriptionGenerator
+    {
+        public string Generate(Product product)
+        {
+            var modifiersTitles = new List<string>();
+            foreach (var group in product.ModifierGroups)
+            {
+                if (group.Modifiers.Length == 0)
+                    continue;
+
+                string modifierString = null;
+                int modifiersLength = group.Modifiers.Length;
+
+                for (int i = 0; i < modifiersLength; i++)
+                {
+                    modifierString += $@"{group.Modifiers[i].Title.Ru}{(i + 1 != modifiersLength ? "/" : null)}";
+                }
+
+                modifiersTitles.Add(modifierString);
+            }
+
+            return $"*Комбо за {product.Price}₽* (ранее {product.OldPrice}₽)\n{string.Join("\n", modifiersTitles.Select((title, i) => $"{i + 1}. {title}"))}\n\n*Купон:* `{product.Coupon}`";
+        }
+    }
+}
