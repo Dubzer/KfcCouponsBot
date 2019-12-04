df00fb0b75bfea747c76d35d6da09eca91013855
Dubzer <dubzer@ya.ru>
2019-12-05 00:12:09 +0300

    Created Cleaner

---
+++ KfcCoupons/Cleaner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using KfcCoupons.Models;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace KfcCoupons
+{
+    public class Cleaner
+    {
+        private readonly TelegramBotClient _client;
+        private readonly ChatId _chatId;
+
+        public Cleaner(TelegramBotClient client,ChatId chatId) =>
+            (_client, _chatId) = (client, chatId);
+
+        public async Task Clean(Dictionary<long, int> messageIdOfProduct, Product[] currentProducts)
+        {
+            
+        }
+    }
+}
