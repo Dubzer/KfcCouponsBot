f1a0673171f53e5187090d4831cbe520a1d480ac
Dubzer <dubzer@ya.ru>
2019-12-08 15:27:56 +0300

    Refactored DescriptionGenerator

--- KfcCoupons/DescriptionGenerator.cs
+++ KfcCoupons/DescriptionGenerator.cs
@@ -9,28 +9,24 @@ namespace KfcCoupons
         public string Generate(Product product)
         {
             var modifiersTitles = new List<string>();
-            foreach (ModifierGroup group in product.ModifierGroups)
+            foreach (ModifierGroup group in product.ModifierGroups.Where(group => group.Modifiers.Length != 0))
             {
-                if (group.Modifiers.Length == 0)
-                    continue;
-
-                string modifierString = null;
                 int modifiersLength = group.Modifiers.Length;
 
-                if (modifiersLength < 3)
-                {
-                    for (int i = 0; i < modifiersLength; i++)
-                        modifierString += $@"{@group.Modifiers[i].Title.Ru}{(i + 1 != modifiersLength ? "/" : null)}";
+                // Using detailed title if there aren't many elements here,
+                // or generic title for a large number of elements
+                string modifierString = modifiersLength < 3
+                    ? string.Join('/',
+                        group.Modifiers.Select((modifier, i) =>
+                            $@"{modifier.Title.Ru}{(i + 1 != modifiersLength ? "/" : null)}"))
+                    : group.Title;
 
-                    modifiersTitles.Add(modifierString);
-                }
-                else
-                {
-                    modifiersTitles.Add(group.Title);
-                }
+                modifiersTitles.Add(modifierString);
             }
 
-            return $"*Комбо за {product.Price}₽* (ранее {product.OldPrice}₽)\n{string.Join("\n", modifiersTitles.Select((title, i) => $"*{i + 1}*. {title}"))}\n\n*Купон:* `{product.Coupon}`";
+            return $"*Комбо за {product.Price}₽* (ранее {product.OldPrice}₽)\n{GetNumberedList(modifiersTitles)}\n\n*Купон:* `{product.Coupon}`";
         }
+
+        private static string GetNumberedList(IEnumerable<string> list) => string.Join("\n", list.Select((title, i) => $"*{i + 1}*. {title}"));
     }
 }
