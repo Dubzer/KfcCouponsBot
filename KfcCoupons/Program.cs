d05cd991cb8eab5164511f9cd6ea5b98546a7c35
Dubzer <dubzer@ya.ru>
2019-12-08 14:55:43 +0300

    Fixed typo + use return instead of Environment.Exit(0)

--- KfcCoupons/Program.cs
+++ KfcCoupons/Program.cs
@@ -19,34 +19,35 @@ namespace KfcCoupons
 
         private static async Task Main()
         {
+            // We use a dynamic type because user can enter an integer(for id) or a string(for nickname) value 
             var chatId = Configuration.GetValue<dynamic>("kfccoupons:chatid");
             var telegramClient = new TelegramBotClient(Configuration.GetValue<string>("telegram:botToken"));
             var descriptionGenerator = new DescriptionGenerator();
             var poster = new Poster(telegramClient, chatId);
             var kfcClient = new KfcClient();
 
-            PostedProduct[] oldProcuts = null;
+            PostedProduct[] oldProducts = null;
             (IEnumerable<Product> newProducts, string newHash) = await kfcClient.GetProductsWithCoupon();
             var postedProducts = new List<PostedProduct>();
 
-            bool firstLaunch = !File.Exists("hash.txt") && !File.Exists("products.json");
-
+            bool firstLaunch = !File.Exists("hash.txt") && !File.Exists("products.json"); 
+            
             if (!firstLaunch)
             {
                 string oldHash = File.ReadAllText("hash.txt");
                 if (oldHash == newHash)
-                    Environment.Exit(0);
+                    return;
 
                 string oldProductsText = await File.ReadAllTextAsync("products.json");
-                oldProcuts = JsonConvert.DeserializeObject<PostedProduct[]>(oldProductsText);
+                oldProducts = JsonConvert.DeserializeObject<PostedProduct[]>(oldProductsText);
 
-                foreach (PostedProduct oldProduct in oldProcuts.Where(p => newProducts.All(x => x.Id != p.Id)))
+                foreach (PostedProduct oldProduct in oldProducts.Where(p => newProducts.All(x => x.Id != p.Id)))
                     await telegramClient.DeleteMessageAsync(chatId, oldProduct.MessageId);
             }
 
             foreach (Product newProduct in firstLaunch
                 ? newProducts
-                : newProducts.Where(p => oldProcuts.All(x => x.Id != p.Id)))
+                : newProducts.Where(p => oldProducts.All(x => x.Id != p.Id)))
             {
                 int messageId = await poster.Post(descriptionGenerator.Generate(newProduct), new Uri($"https://s82079.cdn.ngenix.net/{newProduct.Thumbnail}?dw=250"));
                 postedProducts.Add(new PostedProduct(newProduct.Id, messageId));
