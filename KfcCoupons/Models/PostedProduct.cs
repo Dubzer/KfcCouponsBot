898027073716eb6523764418106f236dcdcd5eeb
Dubzer <dubzer@ya.ru>
2019-12-08 13:02:31 +0300

    Added PostedProduct model

---
+++ KfcCoupons/Models/PostedProduct.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Telegram.Bot.Types;
+
+namespace KfcCoupons.Models
+{
+    public class PostedProduct
+    {
+        public readonly long Id;
+        public readonly int MessageId;
+
+        public PostedProduct(long id, int messageId)
+        {
+            Id = id;
+            MessageId = messageId;
+        }
+    }
+}
